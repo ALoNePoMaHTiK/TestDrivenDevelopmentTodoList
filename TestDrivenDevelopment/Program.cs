736d6e4b9fe19f06f317d5f1190199a8a9d2a60f
ALoNePoMaHTiK <pomahtik11@gmail.com>
2024-11-22 13:41:45 +0300

    Добавьте файлы проекта.

---
+++ TestDrivenDevelopment/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TestDrivenDevelopment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class ToDoListManager
+    {
+        private List<ToDoListItem> ToDoList = new List<ToDoListItem> ();
+
+        public void AddToDoListItem(string taskName)
+        {
+            ToDoList.Add(new ToDoListItem(taskName));
+        }
+        
+        public List<ToDoListItem> GetToDoList()
+        {
+            return ToDoList;
+        }
+
+        public class ToDoListItem
+        {
+            public ToDoListItem(string taskName)
+            {
+                Name = taskName;
+            }
+            public string Name { get; set; } = "Задача";
+            public bool IsCompleted { get; set; } = false;
+        }
+    }
+}
---
+++ TestDrivenDevelopmentTests/ToDoListTests.cs
@@ -0,0 +1,15 @@
+using TestDrivenDevelopment;
+
+namespace TestDrivenDevelopmentTests
+{
+    [TestClass]
+    public class ToDoListTests
+    {
+        private ToDoListManager todoListManager = new ToDoListManager();
+        [TestMethod]
+        public void TestAddToDoListItem()
+        {
+
+        }
+    }
+}
